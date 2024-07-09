de5abef442296239f988df667f383df57368c8a1
Jordan Harband <ljharb@gmail.com>
2024-07-09 14:48:19 -0700

    add linting

--- handler/index.mjs
+++ handler/index.mjs
@@ -1,8 +1,8 @@
 async function getVersion(user, repo) {
 	const url = `https://raw.github.com/${user}/${repo}/HEAD/package.json`;
-  const raw = await fetch(url)
-  const { version } = await raw.json()
-  return version
+	const raw = await fetch(url);
+	const { version } = await raw.json();
+	return version;
 }
 
 async function svgBadge(user, repo) {
@@ -25,7 +25,7 @@ async function svgBadge(user, repo) {
 	const version = await getVersion(user, repoSanitized);
 	if (typeof version !== 'string' || version === '') {
 		throw [422, `valid version not found in package.json: found ${version}`];
-	};
+	}
 	return {
 		headers: {
 			'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
@@ -34,10 +34,10 @@ async function svgBadge(user, repo) {
 		statusCode: 200,
 		body: `<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="10"><text y="9" font-size="12" fill="#2d2d2d" font-family="Arial">v${version}</text></svg>`,
 	};
-};
+}
 
-export async function handler ({pathParameters}) {
-  let { user, repo } = pathParameters
+export async function handler({ pathParameters }) {
+	const { user, repo } = pathParameters;
 	try {
 		return await svgBadge(user, repo);
 	} catch (e) {
@@ -57,4 +57,8 @@ export async function handler ({pathParameters}) {
 			};
 		}
 	}
-};
+	return {
+		statusCode: 418,
+		body: 'i’m a teapot',
+	};
+}
